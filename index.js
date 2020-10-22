65ed0d2c05144f32e0bd922f9a3f313bffa69dae
daiyuanhao <948675173@qq.com>
2020-10-22 18:21:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const { dirname, resolve } = require('path')
+const sharp = require('sharp')
+
+process.ROOT = __dirname
+
+// 创建文件夹方法
+const mkdirSync = (dirname) => {
+  if(!fs.existsSync(dirname)){
+    fs.mkdirSync(dirname)
+  }
+}
+
+// 获取文件大小
+const getFileSize = (dirname) => {
+  return (fs.statSync(dirname).size / 1024).toFixed(2) + 'KB'
+}
+
+// 调整图片大小
+const imgResize = (intoPath, outPath, width) => {
+  return new Promise(resolve => {
+    sharp(intoPath).resize(width).toFile(outPath)
+    resolve()
+  })
+}
+
+const intoPath = `${process.ROOT}/images`
+const outPath = `${process.ROOT}/out`
+mkdirSync(outPath)
+
+const files = fs.readdirSync(intoPath)
+
+// 批量修改图片大小
+const batchImgResize = async () => {
+  for(let i=0;i<files.length;i++){
+    const size = getFileSize(`${intoPath}/${files[i]}`)
+    console.log(`图片大小: ${size}`)
+    await imgResize(`${intoPath}/${files[i]}`, `${outPath}/${files[i]}`, 800)
+    const size2 = getFileSize(`${outPath}/${files[i]}`)
+    console.log(`转换后图片大小: ${size2}`)
+  }
+}
+
+batchImgResize()
