fab9a3887c3617f67ec59aa2185f2650c80ade63
daiyuanhao <948675173@qq.com>
2020-10-23 15:21:42 +0800

    11

---
+++ README.md
@@ -0,0 +1,10 @@
+### img-tool
+
+#### 使用方法
+
+- 克隆项目
+- 进入项目 npm install
+- 在根目录创建images文件夹，将图片放入images文件夹下
+- 在index.js文件下更改图片需要调整的宽，会按比例缩放
+- 执行命令行 img-tool (npm link)
+- out为图片导出目录
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const sharp = require('sharp')
 process.ROOT = __dirname
 
 // 创建文件夹方法
-const mkdirSync = (dirname) => {
+const mkdirSync = async (dirname) => {
   if(!fs.existsSync(dirname)){
     fs.mkdirSync(dirname)
   }
@@ -17,29 +17,24 @@ const getFileSize = (dirname) => {
   return (fs.statSync(dirname).size / 1024).toFixed(2) + 'KB'
 }
 
-// 调整图片大小
-const imgResize = (intoPath, outPath, width) => {
-  return new Promise(resolve => {
-    sharp(intoPath).resize(width).toFile(outPath)
-    resolve()
-  })
-}
-
 const intoPath = `${process.ROOT}/images`
 const outPath = `${process.ROOT}/out`
 mkdirSync(outPath)
 
 
 // 批量修改图片大小
-const batchImgResize = async () => {
+const batchImgResize = async (width) => {
   const files = fs.readdirSync(intoPath)
+  if(files.length ===0) {
+    console.log('当前目录下没有文件')
+  }
   for(let i = 0; i < files.length; i++){
     const size = getFileSize(`${intoPath}/${files[i]}`)
     console.log(`图片大小: ${size}`)
-    await imgResize(`${intoPath}/${files[i]}`, `${outPath}/${files[i]}`, 800)
+    await sharp(`${intoPath}/${files[i]}`).resize(width).toFile(`${outPath}/${files[i]}`)
     const size2 = getFileSize(`${outPath}/${files[i]}`)
     console.log(`转换后图片大小: ${size2}`)
   }
 }
 
-batchImgResize()
+batchImgResize(800)
